6fb662d4bc42d9cb5fdf97feda91b254ea6bce85
choisangwon <rngus3698@naver.com>
2023-07-07 15:51:43 +0900

    코드 정리

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,24 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { ConfigService } from '@nestjs/config';
-import { Configuration } from './config/configuration.interface';
 
 @Controller()
 export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    private configService: ConfigService,
-  ) {}
+  constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
-    const port = this.configService.get<number>('port');
-    const node_env = this.configService.get<string>('node_env');
-    const database =
-      this.configService.get<Configuration['database']>('database');
-    console.log(node_env);
-    console.log(port);
-    console.log(database);
     return this.appService.getHello();
   }
 }
