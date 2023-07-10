d9d40aa128dd0b372eacd4085e241dd068dd2f47
choisangwon <rngus3698@naver.com>
2023-07-10 19:13:23 +0900

    response.utils.ts message 노출 로직 수정

--- src/util/response.util.ts
+++ src/util/response.util.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import message from '../utils/response/message';
 
 @Injectable()
 export class ResponseUtil {
@@ -7,6 +8,32 @@ export class ResponseUtil {
   private resCode: string;
   private message: string;
   private body: any;
+
+  messageUtil(resCode) {
+    const message = {
+      '0000': '성공',
+      '0001': '회원을 찾을 수 없습니다.',
+      '0002': '지급할 쿠폰을 찾을 수 없습니다.',
+      '0003': '쿠폰을 지급할 회원이 없습니다.',
+      '0004': '비밀번호가 일치하지 않습니다.',
+
+      // status 400
+      '4000': '파라미터 유효성 검증 실페',
+
+      // status 401
+      '8999': '토큰 형식이 올바르지 않습니다.',
+      '8998': '토큰 type이 올바르지 않습니다.',
+      '8997': '토큰으로 회원을 찾을 수 없습니다.',
+      '8996': '탈퇴한 회원입니다.',
+
+      // status 409
+
+      // status 500
+      '9999': '서버오류',
+      '9998': 'DB오류',
+    };
+    return message[resCode];
+  }
 
   response(
     res: any,
@@ -18,8 +45,14 @@ export class ResponseUtil {
     this.res = res;
     this.statusCode = statusCode;
     this.resCode = resCode;
-    this.message = message;
     this.body = body;
+    this.message =
+      message === ''
+        ? this.messageUtil(resCode) !== undefined
+          ? this.messageUtil(resCode)
+          : ''
+        : message;
+
     return this.res
       .status(this.statusCode)
       .json({ resCode: this.resCode, message: this.message, body: this.body });
