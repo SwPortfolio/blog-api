29b4906769bffed93d4a15cccda7c9a4db9983c6
choisangwon <rngus3698@naver.com>
2023-07-10 15:04:28 +0900

    database 모듈 전역으로 사용 root 모듈에 한번만 등록

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,6 +1,7 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { DatabaseService } from './database.service';
 
+@Global() // @Global() 데코레이터는 모듈을 전역적으로 사용할 수 있도록 만듭니다. 전역 모듈은 일반적으로 루트 또는 코어 모듈에 의해 단 한 번만 등록되어야 합니다.
 @Module({
   providers: [DatabaseService],
   exports: [DatabaseService],
--- src/member/member.module.ts
+++ src/member/member.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MemberController } from './member.controller';
-import { DatabaseModule } from '../database/database.module';
 import { MemberModel } from './member.model';
 import { MemberService } from './member.service';
 
 @Module({
-  imports: [DatabaseModule],
+  // imports: [DatabaseModule],
   controllers: [MemberController],
   providers: [MemberService, MemberModel],
   exports: [MemberService],
